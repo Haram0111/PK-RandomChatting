e8fd6501d4087facb85e8b6662b580937dc9209f
Haram0111 <5gkfka5@naver.com>
2022-10-03 16:13:02 +0900

    Pages

---
+++ src/Page/SingupPage2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SignupDetail from "../Component/SignupDetail";
+import Background from "../Atom/Background";
+import Title from "../Component/title";
+
+function SingupPage1() {
+  return (
+    <Background>
+      <Title/>
+      <SignupDetail/>
+    </Background>
+  );
+}
+
+export default SingupPage1;
