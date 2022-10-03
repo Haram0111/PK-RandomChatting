54517c1f114ee1c4250e3d55d4fa4fae672c47d8
Haram0111 <5gkfka5@naver.com>
2022-10-03 14:41:10 +0900

    A: 로그인에 들어가는 버튼

---
+++ src/Atom/SignUpButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    margin-top: 15px;
+    height: 50px;
+    width: 70%;
+    background-color: #002060;
+    color: white;
+    font-size: 32px;
+    border-radius: 10px;
+`;
+
+function SignUpButton() {
+  return (
+    <StyledButton>Sign Up</StyledButton>
+  );
+}
+
+export default SignUpButton;
