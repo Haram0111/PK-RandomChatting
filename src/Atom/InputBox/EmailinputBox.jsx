ab60b545ea60d10e8fb7d42b5f1af10a69665592
Haram0111 <5gkfka5@naver.com>
2022-10-03 16:12:38 +0900

    InBoxs

---
+++ src/Atom/InputBox/EmailinputBox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+    height: 30px;
+    width: 70%;
+    background-color: #DFE6F4;
+    color: #000000;
+    font-size: 16px;
+    border-radius: 10px;
+`;
+
+function IDinputBox() {
+    return (
+        <StyledInput placeholder="  Backkyung@pknukong.ac.kr"></StyledInput>
+    );
+}
+
+export default IDinputBox;
---
+++ src/Atom/InputBox/PasswordinputBox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+    height: 30px;
+    width: 70%;
+    background-color: #DFE6F4;
+    color: #000000;
+    font-size: 16px;
+    border-radius: 10px;
+`;
+
+function PasswordInputBox() {
+    return (
+        <StyledInput></StyledInput>
+    );
+}
+
+export default PasswordInputBox;
