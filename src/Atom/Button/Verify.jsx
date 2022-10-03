c04dbdbd17be7c05af2dab488782e72543015fe7
Haram0111 <5gkfka5@naver.com>
2022-10-03 16:12:02 +0900

    Buttons

---
+++ src/Atom/Button/Verify.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    margin-top: 15px;
+    height: 50px;
+    width: 70%;
+    background-color: #002060;
+    color: white;
+    font-size: 32px;
+    border-radius: 10px;
+`;
+
+function Verity() {
+  return (
+    <StyledButton>Verity</StyledButton>
+  );
+}
+
+export default Verity;
